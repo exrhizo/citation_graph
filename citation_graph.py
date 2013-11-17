5a1623cb650e0d9c20db6d4e3dc30f3ec51095a3
ian borukhovich <ianboru@gmail.com>
2013-11-17 11:41:16 -0700

    fixed extractionnotallowed error

--- citation_graph.py
+++ citation_graph.py
@@ -1,10 +1,11 @@
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
+#from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+
 # Open a PDF file.
 fp = open('2009 Bellesia Local vs Global propensity.pdf', 'rb')
 # Create a PDF parser object associated with the file object.
@@ -13,10 +14,12 @@ parser = PDFParser(fp)
 document = PDFDocument(parser)
 # Supply the password for initialization.
 # (If no password is set, give an empty string.)
+password = ""
 document.initialize(password)
 # Check if the document allows text extraction. If not, abort.
 if not document.is_extractable:
-    raise PDFTextExtractionNotAllowed
+	print "CANT"
+#    raise PDFTextExtractionNotAllowed
 # Create a PDF resource manager object that stores shared resources.
 rsrcmgr = PDFResourceManager()
 # Create a PDF device object.
@@ -26,5 +29,5 @@ interpreter = PDFPageInterpreter(rsrcmgr, device)
 # Process each page contained in the document.
 for page in PDFPage.create_pages(document):
     interpreter.process_page(page)
-    print len(page)
+    print page.__doc__
     
